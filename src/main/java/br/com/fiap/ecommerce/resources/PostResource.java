9d9d3278fdfe5c2374b3b438d51eaa56f9db5fc5
wees-guimaraes <wees.guimaraes@gmail.com>
2022-02-02 18:28:08 -0300

    Obtendo posts por id

---
+++ src/main/java/br/com/fiap/ecommerce/resources/PostResource.java
@@ -0,0 +1,27 @@
+package br.com.fiap.ecommerce.resources;
+
+import br.com.fiap.ecommerce.domain.Post;
+import br.com.fiap.ecommerce.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = service.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+
+
+}
---
+++ src/main/java/br/com/fiap/ecommerce/services/PostService.java
@@ -0,0 +1,26 @@
+package br.com.fiap.ecommerce.services;
+
+import br.com.fiap.ecommerce.domain.Post;
+import br.com.fiap.ecommerce.repository.PostRepository;
+import br.com.fiap.ecommerce.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id){
+        Optional<Post> optPost = repository.findById(id);
+
+        return optPost.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado"));
+    }
+
+
+
+
+}
