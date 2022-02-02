f346713dc9f865430be918fe7c8cd30fa90ccf05
wees-guimaraes <wees.guimaraes@gmail.com>
2022-02-02 19:23:26 -0300

    Acrescentando comentários aos posts.

--- src/main/java/br/com/fiap/ecommerce/config/Instantiation.java
+++ src/main/java/br/com/fiap/ecommerce/config/Instantiation.java
@@ -3,6 +3,7 @@ package br.com.fiap.ecommerce.config;
 import br.com.fiap.ecommerce.domain.Post;
 import br.com.fiap.ecommerce.domain.User;
 import br.com.fiap.ecommerce.dto.AuthorDTO;
+import br.com.fiap.ecommerce.dto.CommentDTO;
 import br.com.fiap.ecommerce.repository.PostRepository;
 import br.com.fiap.ecommerce.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -22,7 +25,7 @@ public class Instantiation implements CommandLineRunner {
     private PostRepository postRepository;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         userRepository.deleteAll();
         postRepository.deleteAll();
@@ -35,13 +38,26 @@ public class Instantiation implements CommandLineRunner {
 
         AuthorDTO authorDTO1 = new AuthorDTO(maria);
 
+        CommentDTO comment1 = new CommentDTO("Boa viagem, mano!", LocalDate.now(), new AuthorDTO(alex));
+
+        CommentDTO comment2 = new CommentDTO("Aproveite!", LocalDate.now(), new AuthorDTO(bob));
+
+        List<CommentDTO> listComments1 = new ArrayList<>(Arrays.asList(comment1, comment2));
+
+        List<CommentDTO> listComments2 = new ArrayList<>();
+
+        CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", LocalDate.now(), new AuthorDTO(alex));
+
+        listComments2.add(comment3);
+
         Post post1 = new Post();
         post1.setDate(LocalDate.of(2021, 11, 24));
         post1.setTitle("Partiu viagem");
         post1.setBody("Vou viajar para São Paulo. Abraços!");
         post1.setAuthor(authorDTO1);
+        post1.setCommentDTOList(listComments1);
 
-        Post post2 = new Post(null, "Bom dia", "Acordei Feliz hoje!", LocalDate.of(2022, 2, 1), authorDTO1);
+        Post post2 = new Post(null, "Bom dia", "Acordei Feliz hoje!", LocalDate.of(2022, 2, 1), authorDTO1, listComments2);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/br/com/fiap/ecommerce/domain/Post.java
+++ src/main/java/br/com/fiap/ecommerce/domain/Post.java
@@ -1,17 +1,20 @@
 package br.com.fiap.ecommerce.domain;
 
 import br.com.fiap.ecommerce.dto.AuthorDTO;
+import br.com.fiap.ecommerce.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
 public class Post implements Serializable {
 
-    private static final long serialVersionUID = 1l;
+    private static final long serialVersionUID = 1L;
 
     @Id
     private String id;
@@ -21,15 +24,18 @@ public class Post implements Serializable {
 
     private AuthorDTO author;
 
+    private List<CommentDTO> commentDTOList = new ArrayList<>();
+
     public Post() {
     }
 
-    public Post(String id, String title, String body, LocalDate date, AuthorDTO authorDTO) {
+    public Post(String id, String title, String body, LocalDate date, AuthorDTO author, List<CommentDTO> commentDTOList) {
         this.id = id;
         this.title = title;
         this.body = body;
         this.date = date;
-        this.author = authorDTO;
+        this.author = author;
+        this.commentDTOList = commentDTOList;
     }
 
     @Override
@@ -75,6 +81,14 @@ public class Post implements Serializable {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public List<CommentDTO> getCommentDTOList() {
+        return commentDTOList;
+    }
+
+    public void setCommentDTOList(List<CommentDTO> commentDTOList) {
+        this.commentDTOList = commentDTOList;
     }
 
     @Override
